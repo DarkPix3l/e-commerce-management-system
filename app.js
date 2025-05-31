720c5dad210e50a4e89e7f934574f4e608e3925b
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-05-31 12:53:14 +0200

    installed required libraries, created folders, created server, defined port, env variables, configured database start function

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express');
+import { API_URL } from "./config/variable.js";
+import { startDatabase } from './config/db.js';
+import express from "express";
+
+//creating server
 const app = express();
+app.use(express.json());
 
 
-app.get('/', (req, res) =>{
+const PORT = 3000;
+startDatabase();
+
+//we need to have an api and a version in the URL of the route. create that with env.var
+//want a public var for API URL wich will be prefixed for every route i use for my app
+//http://localhost:3000/api/v1/products
+app.get(API_URL+'/products', (req, res) =>{
     res.send('hello API!');
 })
 
-/* listening to */
-app.listen(3000, ()=> {
-    console.log('server is running http://localhost:3000');
+
+
+/* running the server on */
+app.listen(PORT, ()=> {
+    console.log(`server is running at ${PORT}`);
 })
--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import { MONGO_URL } from "./variable.js";
 
 export const startDatabase = async () =>{
    try {
-     await mongoose.connect("");
+     await mongoose.connect(MONGO_URL);
      console.log("connected");
    } catch (error) {
     console.log("not connectded", error.message);
