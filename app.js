e76f9be971785876703a5f2d705e3820248e6898
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-06-02 14:46:07 +0200

    Add request logging with Morgan, created new .db and added its name to the connection string. Refined GET and POST requests.

--- app.js
+++ app.js
@@ -1,34 +1,56 @@
 import { API_URL } from "./config/variable.js";
-import { startDatabase } from './config/db.js';
+import { startDatabase } from "./config/db.js";
 import express from "express";
-
+import morgan from "morgan";
+import productSchema from "./models/products.js";
 
 //creating server
 const app = express();
+//Parse in json format
 app.use(express.json());
-
+app.use(morgan("tiny"));
 
 const PORT = 3000;
 startDatabase();
 
+app.get(`${API_URL}/products`, async (req, res) => {
+//basic fetching
+   try {
+    let products = await productSchema.find();
+    return res.send(products);
+  } catch (error) {
+    return res.status(500).send("Server Error");
+  }
+ 
+});
 
-app.get(`${API_URL}/products`, (req, res) =>{
-    const product ={
-        id:1,
-        name: 'hair dresser',
-        image:'someurl'
-    }
-    res.send(product);
+app.post(`${API_URL}/products`, async (req, res) => {
+  /* console.log(productSchema);
+  res.send(productSchema); */
+
+  // Create a new object that follow its schema
+  const whatever = await productSchema.create({
+    title: "Example Product2",
+    slug: "example-product2",
+    description: "This is a description of the example product.",
+    price: 18.99,
+    instock: true,
+    quantity: 15,
+    imageUrl: "http://example.com/image2.jpg",
+  });
+
+whatever.save().then((createdProduct=>{
+  res.status(201).json(createdProduct)
+})).catch((err)=>{
+  res.status(500).json({
+    error: err,
+    success:false
+  })
 })
 
-app.post(`${API_URL}/products`, (req, res) =>{
-    const newProduct = req.body;
-    res.send(newProduct);
-    console.log(newProduct);
-})
-
+});
 
 /* running the server on */
-app.listen(PORT, ()=> {
-    console.log(`server is running at ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ import { MONGO_URL } from "./variable.js";
 export const startDatabase = async () =>{
    try {
      await mongoose.connect(MONGO_URL);
-     console.log("connected");
+     console.log("connected to database");
    } catch (error) {
-    console.log("not connectded", error.message);
+    console.log("not connectded to database", error.message);
    }
 }
